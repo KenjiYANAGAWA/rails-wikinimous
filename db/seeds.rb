7b82a348f542a9951794301e0b6d276bef055385
Kenji Yanagawa <kenchibi@gmail.com>
2022-05-10 21:28:01 +0900

    created db and model. seed done to. next step: routes controller view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p 'clearing db'
+Article.destroy_all
+10.times do
+  new_article = Article.new(
+    title: Faker::Quote.famous_last_words,
+    content: Faker::Lorem.paragraph(sentence_count: 30)
+  )
+  new_article.save
+end
+p "now there are #{Article.count} articles"
